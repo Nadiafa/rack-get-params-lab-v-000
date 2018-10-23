1f085b0c9c5ac62e7cf6ef446fedafb05d951d21
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 10:35:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
         resp.write "Your cart is empty"
       else
         @@cart.each do |i|
-          resp.write "- #{i}\n"
+          resp.write "#{i}\n"
         end
       end
     
