29c9191a55eeab7843dcf0bdde61a4144a9aa118
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 10:30:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,9 +14,13 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    
+    
     elsif req.path.match(/cart/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    
+    
     else
       resp.write "Path Not Found"
     end
