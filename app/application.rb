d3144464c311050f48e40c53607e232fcc29463b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 10:44:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,24 +23,14 @@ class Application
           resp.write "#{i}\n"
         end
       end
-      
-      
-      elsif req.path.match(/add/)
-      search_term = req.params["item"]
-        if @@items.include? search_term
-          @@cart << search_term
-          resp.write "added #{search_term}"
-        else
-          resp.write "We don't have that item!"
-        end
-
-
-
-      
-      
-      
-      
-    
+    elsif req.path.match(/add/)
+    search_term = req.params["item"]
+      if @@items.include? search_term
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
